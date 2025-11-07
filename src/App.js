48470bde3df714599926b88c5b0be7d153113b6f
choigaeul <choigaeul@suwon.ac.kr>
2025-11-07 22:33:04 +0900

    feat: 카드 리스트 컴포넌트 기능 구현 완료(테일윈드 미적용)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import CardList from './Card_list/Card_list_ver1.jsx';
 import CardList2 from './Card_list/Card_list_ver2.jsx';
 import './App.css';
 import './index.css';
-import Card from './Card/Card.jsx';
 
 function App() {
   return (
     <>
       <CardList />
       <CardList2 />
-      
-      <Card />
     </>
   );
 }
