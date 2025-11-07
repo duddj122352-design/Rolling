c7f0faecdd2e54d42fca2e234d76aaefc36b92c0
duddj122352-design <duddj122352@gmail.com>
2025-11-07 15:32:53 +0900

    feat: 헤더 컴포넌트 기능 추가 및 의존성 업데이트

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 function Header() {
-  return (
-    <></>
-  )
+  return(
+    <>
+    <header className='bg-white sticky top-0 z-50 text-20-bold'>
+      <div className="flex items-center justify-between px-4">
+        {/* Rolling 로고 */}
+        <div className='flex items-center space-x-2'>
+          <span className="text-xl text-purple-600"></span>
+          <span className="text-xl font-semibold text-gray-800">Rolling</span>
+        </div>
+      </div>
+    </header>
+    </>
+  );
 }
 
 export default Header
