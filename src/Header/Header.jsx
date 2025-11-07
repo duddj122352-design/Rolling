ebd345bd56c1e7bece58067f4b40188c03b412fd
duddj122352-design <duddj122352@gmail.com>
2025-11-07 18:26:05 +0900

    feat

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { ReactComponent as RollingIcon } from './img/logo.svg';
+import RollingIcon from './img/logo.png';
 
 function Header() {
   return (
     <>
-      <header className='bg-white sticky top-0 z-50'>
-        <div className="flex items-center justify-between px-6">
+      <header className='bg-white sticky top-0 z-50 w-full h-[64px]'>
+        <div className="w-full h-full flex items-center justify-between">
           {/* Rolling 로고 영역 */}
-          <div className='flex items-center space-x-2'>
+          <div className='flex items-center justify-center justify-items-center z-60'>
             {/* 로고 아이콘 */}
-            <a href="/"><RollingIcon className="w-6 h-6" /></a>
+            <a href="/"><img src={RollingIcon} alt="로고" /></a>
           </div>
 
           {/* "롤링 페이퍼 만들기" 버튼 영역 */}
-          <button
-            className="px-4 py-2 bg-white rounded-lg border grayscale-300 text-16-bold">
-            롤링 페이퍼 만들기
-          </button>
+          <div>
+            <button
+              className="border bg-white rounded-lg  text-16-bold h-[42px] w-[149px]">
+              롤링 페이퍼 만들기
+            </button>
+          </div>
         </div>
       </header>
     </>
