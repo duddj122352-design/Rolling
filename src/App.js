68f55e79b56529d8c6c02e8bfea58bf4c4b262f7
Oh Young gyo <oey157@nate.com>
2025-11-09 14:54:54 +0900

    emoji

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Card from './Card/Card.jsx';
-import AddCard from './Card/AddCard.jsx';
 
 
 function App() {
   return (
     <>
-      <Card />
-      <AddCard />
     </>
 
 
