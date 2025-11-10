d1e46cc7c4b3977b7ac1dd1679def64712d25219
choigaeul <choigaeul@suwon.ac.kr>
2025-11-10 16:32:49 +0900

    pull 전 수정사항 반영

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Card from './Card/Card.jsx';
 import AddCard from './Card/AddCard.jsx';
+import CardList from './Card_list/Card_list_ver1.jsx';
+import CardList2 from './Card_list/Card_list_ver2.jsx';
 
 function App() {
   return (
     <>
       <Card />
       <AddCard />
+      <CardList />
+      <CardList2 />
     </>
 
 
